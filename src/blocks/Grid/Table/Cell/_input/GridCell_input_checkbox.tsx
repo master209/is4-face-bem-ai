335cafdd8a538693d42d86ba3822c3ecaa943ec7
chizhov.in <chizhov.in@gals-telecom.ru>
2026-03-12 16:54:37 +0400

    ремонт кликабельного чекбокса в ячейке блока Grid - 2

--- src/blocks/Grid/Table/Cell/_input/GridCell_input_checkbox.tsx
+++ src/blocks/Grid/Table/Cell/_input/GridCell_input_checkbox.tsx
@@ -7,16 +7,19 @@ import { InputCheckbox } from '../../../../InputCheckbox';
 import { cnGrid } from '../../..';
 import { IGridCellProps } from '../Grid-Cell';
 
-// модификатор для ячеек таблицы, отображаемых как контрол checkbox
+// Модификатор для ячеек таблицы, отображаемых как контрол checkbox
+// Данный компонент превращает значение булева поля в управляемый чекбокс.
 export const withGridCellInputCheckbox = withBemMod<IGridCellProps>(
   'GridCell',
   { inputType: 'checkbox' }, // тип контрола для этого модификатора
-  // eslint-disable-next-line react/display-name
   (GridCell) => ({...props}: IGridCellProps) => {
     const {row, field, inputs} = props;
-    const isChecked = () => row[field] === 'да';
     const [isLoading, setIsLoading] = useState(false);
 
+    // если булево поле не превращаем в чекбокс, то в ячейке выводится 'да'/'нет'
+    const isChecked = () => row[field] === 'да';
+
+    // преобразование в 'да' или 'нет' в зависимости от цифрового значения поля
     const numYesNo = (num: number) => num > 0 ? 'да' : 'нет';
 
     const handleChange = async (ev: ChangeEvent, checked: boolean) => {
@@ -25,7 +28,7 @@ export const withGridCellInputCheckbox = withBemMod<IGridCellProps>(
         const response = await api.put(`${inputs[field].apiHandler}/${row.id}`, {
           [field]: checked ? 0 : 1
         });
-        row[field] = numYesNo(response.data[field]);
+        row[field] = numYesNo(response.data[field]); // установка значения с сервера
       } catch (error) {
         console.error('Error updating checkbox:', error);
         // В случае ошибки можно показать уведомление пользователю
